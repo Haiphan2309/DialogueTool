d8ca6ddcd7b605b562df30b6be5b0ae29545267b
Haiphan2309 <haiphan.230903@gmail.com>
2025-03-17 22:56:55 +0700

    + Add editor text box config

--- DialogueTool/Assets/DialogueSystem/Scripts/Config/TextBoxConfig.cs
+++ DialogueTool/Assets/DialogueSystem/Scripts/Config/TextBoxConfig.cs
@@ -1,8 +1,12 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TextBoxType
+{
+    NORMAL,
+    LOUD,
+}
 public enum PivotType
 {
     NONE,
@@ -14,14 +18,50 @@ public enum PivotType
 [Serializable]
 public class TextBoxPivotConfig
 {
+    public TextBoxType TextBoxType;
     public PivotType PivotType;
     public float DegreeZ;
     public Vector2 AnchorPos;
 }
 
 [Serializable]
+public class PreferHorizontalConfig
+{
+    public int MinTextLength;
+    public float HorizontalSize;
+}
+
+[Serializable]
 [CreateAssetMenu(menuName = "Config/DialogueSystem/TextBoxConfig")]
 public class TextBoxConfig : ScriptableObject
 {
+    public Vector2 MinSize;
+    public Vector2 MaxSize;
+    public float PivotSize;
     public List<TextBoxPivotConfig> TextBoxPivotConfigs;
+
+    [Header("Sort ascending by minTextLength")]
+    [SerializeField] private List<PreferHorizontalConfig> preferHorizontalConfigs;
+
+    public float GetPreferHorizontalSize(string text)
+    {
+        float result = MinSize.x;
+        foreach (var config in preferHorizontalConfigs)
+        {
+            if (text.Length > config.MinTextLength)
+            {
+                result = config.MinTextLength;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return result;
+    }    
+
+    public List<PreferHorizontalConfig> GetPreferHorizontalConfigs()
+    {
+        return preferHorizontalConfigs;
+    }
 }
---
+++ DialogueTool/Assets/DialogueSystem/Scripts/Editor/TextBoxConfigEditor.cs
@@ -0,0 +1,45 @@
+using UnityEditor;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(TextBoxConfig))]
+public class TextBoxConfigEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TextBoxConfig config = (TextBoxConfig)target;
+
+        DrawDefaultInspector();
+
+        if (config.MinSize.x > config.MaxSize.x || config.MinSize.y > config.MaxSize.y)
+        {
+            EditorGUILayout.HelpBox("MinSize can not be larger than MaxSize!", MessageType.Error);
+        }
+
+        if (config.MinSize.x < 0 || config.MinSize.y < 0 || config.MaxSize.x < 0 || config.MaxSize.y < 0)
+        {
+            EditorGUILayout.HelpBox("MinSize or MaxSize can not be negative!", MessageType.Error);
+        }
+
+        if (config.PivotSize < 0)
+        {
+            EditorGUILayout.HelpBox("PivotSize can not be negative!", MessageType.Error);
+        }
+
+        if (!CheckAscending(config.GetPreferHorizontalConfigs()))
+        {
+            EditorGUILayout.HelpBox("preferHorizontalConfigs.MinTextLength need to be sort ascending!", MessageType.Warning);
+        }
+    }
+    
+    private bool CheckAscending(List<PreferHorizontalConfig> preferHorizontalConfigs)
+    {
+        for (int i = 1; i < preferHorizontalConfigs.Count; i++)
+        {
+            if (preferHorizontalConfigs[i].MinTextLength < preferHorizontalConfigs[i - 1].MinTextLength)
+            {
+                return false;
+            }
+        }
+        return true;
+    }    
+}
